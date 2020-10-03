481074ae53361dc37e6b70eaaadeb28658dbe76e
shield02 <myshield02@gmail.com>
2020-10-03 10:44:44 +0100

    Add check_cpu_constrained function and corresponding error messages

--- all-checks.py
+++ all-checks.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import sys
 import socket
+import psutil
 
 def check_reboot():
     """Returns True if the computer has pending reboot."""
@@ -24,6 +25,10 @@ def check_root_full():
     """Returns True if the root partition is full, False otherwise"""
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
+def check_cpu_constrained():
+    """Return True if the cpu is having too much usage, False otherwise"""
+    return psutil.cpu_percent(1) > 75
+
 def check_no_network():
     """Return True if it fails to resolve Google's URL, False otherwise"""
     try:
@@ -35,9 +40,10 @@ def check_no_network():
 
 def main():
     checks=[
-        (check_reboot, "Pending Reboot"),
-        (check_root_full, "Root partition full"),
-        (check_no_network, "No working network"),
+        (check_reboot, "Pending Reboot."),
+        (check_root_full, "Root partition full."),
+        (check_cpu_constrained, "CPU load too high."),
+        (check_no_network, "No working network."),
     ]
     everything_ok= True
     for check, msg in checks:
